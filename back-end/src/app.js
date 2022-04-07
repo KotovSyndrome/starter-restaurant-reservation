751c8f3193a181b408133c8f46e46934de75308c
Payam Katoozian <einpanzerattacken@gmail.com>
2022-04-06 23:52:00 -0700

    Migrated new schema to databas + fixed CORS issue

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -11,7 +11,11 @@ const reservationsRouter = require("./reservations/reservations.router");
 
 const app = express();
 
-app.use(cors());
+app.use( (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.json());
 
 app.use("/reservations", reservationsRouter);
